1746dbe4667c0313b872b2329640df72a3388a0d
邓浩 <dh19900729@163.com>
2017-10-14 14:18:44 +0800

    商品分类页基本完成

--- js/shop-list.js
+++ js/shop-list.js
@@ -1,29 +1,84 @@
-var oCat = document.querySelector('#cat');
-myajax.get('http://h6.duchengjiu.top/shop/api_cat.php', {}, function(error, responseText){
-    var json = JSON.parse(responseText);
-    var data = json.data;
-    for (var i = 0; i < data.length; i++) {
-        var obj = data[i];
-        oCat.innerHTML += `<li><a href="02-商品列表.html?cat_id=${obj.cat_id}">${obj.cat_name}</a></li>`;
-    }
-});
- 
-
-
-var oGoods = document.querySelector('#hot-goods');
-myajax.get('http://h6.duchengjiu.top/shop/api_goods.php', {}, function(err,responseText){
-    var json = JSON.parse(responseText);
-    console.log(json);
-    var data = json.data;
-    for (var i = 0; i < data.length; i++) {
-        var obj = data[i];
-        oGoods.innerHTML += `<li><a href="08-goods.html?goods_id=${obj.goods_id}">
+//请求商品列表
+//var oCat = document.querySelector('#cat');
+var shoplist = "";
+$.get("http://h6.duchengjiu.top/shop/api_cat.php",{},function  (json) {
+	var data = json.data;
+	for(var i=0;i<data.length;i++){
+		var obj = data[i];
+		shoplist += `<li><a href="shop-list.html?goods_id=${obj.goods_id}">${obj.cat_name}</a></li>`;
+	}
+	$("#cat").append(shoplist);
+})
+//商品列表添加点击事件
+$("#cat li").on("click",function  () {
+	$("#cat a").css({"background":"white","color":"skyblue"});
+	$(this).css({"background":"blue","color":"white"})
+})
+//获取cat_id
+$.getQueryShopList = function(name) {
+  var search = location.search.substr(1);
+  var reg = new RegExp('(&|^)'+name+'=([^&]*)(&|$)');
+  var r = search.match(reg);
+  if (r === null) return null;
+  return decodeURI(r[2]);
+};
+//判断是否有cat_id
+var shopCatId;
+if ($.getQueryShopList("cat_id") === null) {
+	shopCatId = 45;
+} else{
+	shopCatId = $.getQueryShopList("cat_id");
+};
+console.log(shopCatId);
+//列表商品
+var listShop = "";
+$.get("http://h6.duchengjiu.top/shop/api_goods.php",{"cat_id":shopCatId,"page":1},function  (json) {
+	var data = json.data;
+	for (var i=0;i<data.length;i++) {
+		var obj = data[i];
+		listShop += `<li class="products"><a href="details.html?goods_id=${obj.goods_id}">
           <div><img src="${obj.goods_thumb}" /></div>
-          <div>${obj.goods_name}</div>
-          <div>${obj.price}</div></a>
+          <div class="goodname">${obj.goods_name}</div>
+          <div class="goodprice">¥${obj.price}</div></a>
         </li>`;
-    }
+	}
+	$("#hot_goods").append(listShop);
+})
+//加载更多
+var pages = 1;
+$("#loadmore").on("touchstart",function() {
+	var strmore = ""
+	++pages;
+	console.log(pages);
+	$.get("http://h6.duchengjiu.top/shop/api_goods.php",{"cat_id":shopCatId,"page":pages},function(json) {
+		console.log(json);
+		var data = json.data;
+		if (data.length === 0) {
+			$("#loadmore").css("display","none");
+			$("#moreover").css("display","block");
+		} else{
+			for (var i=0;i<data.length;i++) {
+			var obj = data[i];
+			strmore +=`<li class="products"><a href="details.html?cat_id=${obj.cat_id}">
+          <div><img src="${obj.goods_thumb}" /></div>
+          <div class="goodname">${obj.goods_name}</div>
+          <div class="goodprice">¥${obj.price}</div></a>
+        </li>`;
+		};
+		$("#hot_goods").append(strmore);
+		};
+	});
 });
+//等待效果
+function loading (){
+	$("#loading").hide();
+	$(document).ajaxStart(function  () {
+		$("#loading").show();
+	}).ajaxStop(function  () {
+		$("#loading").hide();
+	})
+};
+loading();
   
   
 
